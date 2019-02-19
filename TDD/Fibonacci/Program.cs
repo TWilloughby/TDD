ba615c900511e7d7a6a4edd89e0a12c8754fd1b3
TWilloughby <Thomas.Willoughby@gmail.com>
2019-02-19 11:48:57 -0500

    Huh

---
+++ TDD/Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fibberly Gibbet!");
+            if (Debugger.IsAttached)
+                Console.ReadKey();
+        }
+    }
+}
---
+++ TDD/MS-TDD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MS_TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (Debugger.IsAttached)
+                Console.ReadKey();
+        }
+    }
+}
